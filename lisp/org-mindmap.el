8d32a92339e06d0372d83520a88b71b489318d0d
Peter Jones <pjones@pmade.com>
2010-02-16 08:52:25 -0700

    Convert to Graphviz DOT format

--- lisp/org-mindmap.el
+++ lisp/org-mindmap.el
@@ -1,6 +1,6 @@
-;;; org-mindmap.el --- Turn OrgMode Headings Into TikZ Mindmaps
+;;; org-mindmap.el --- Turn OrgMode Headings Into Graphviz (DOT) Mindmaps
 ;;
-;; Copyright (C) 2009 pmade inc. (Peter Jones pjones@pmade.com)
+;; Copyright (C) 2009-2010 pmade inc. (Peter Jones pjones@pmade.com)
 ;;
 ;; Permission is hereby granted, free of charge, to any person obtaining
 ;; a copy of this software and associated documentation files (the
@@ -25,23 +25,22 @@
 ;;
 ;; M-x org-export-as-mindmap
 ;;
-;; That command will export the current subtree as a mindmap.
-;; Actually, it will generated the LaTeX/TikZ needed to create the
-;; mindmap, and save it to a file.  For now, you'll need to compile
-;; the LaTeX yourself.
+;; Generates a Graphviz DOT file starting with the current node and
+;; working downwards.
 ;;
+;; You can control the style of the node (e.g. shape, color, etc.)
+;; using the org-mindmap-node-styles customization or by setting the
+;; DOT_NODE_STYLE property on a heading.
 ;;
-;; TODO: Figure out how to achieve a better layout so that nodes
-;;       aren't all over the place.
+;; For now, you must manually convert the DOT file to something more
+;; useful.  On my system there are three ways to do that:
 ;;
-;; TODO: Add support for plain text items.  Maybe draw lines off their
-;;       parent node more like a traditional mindmap.
+;;  1. Use Graphviz: twopi -Tpng generated.dot > generated.png
 ;;
-;; TODO: Automatically generate a cropped PDF and remove intermediate
-;;       files.
+;;  2. Use dot2tex: http://www.fauskes.net/code/dot2tex/
 ;;
-;; TODO: Figure out how to apply styles so that we can use global
-;;       styles but also provide overrides in org properties.
+;;  3. Open the file in OmniGraffle: 
+;;     http://www.omnigroup.com/applications/omnigraffle/
 (eval-when-compile
   (require 'org))
 
@@ -50,46 +49,29 @@
   :tag "Org-Mindmap" :group 'org)
 
 (defcustom org-mindmap-header
-  "\\documentclass[landscape,letterpaper]{article}
-\\usepackage{tikz}
-\\usetikzlibrary{mindmap,trees}
-\\pagestyle{empty}
-\\begin{document}"
-  "The LaTeX header used in mindmaps."
+  "digraph mindmap {\n  ranksep=3;\n  ratio=auto;\n"
+  "Text inserted at the beginning of the DOT file"
   :type 'string :group 'org-mindmap)
 
 (defcustom org-mindmap-footer
-  "\\end{document}"
-  "The LaTeX footer used in mindmaps."
+  "}\n"
+  "Text inserted at the end of the DOT file"
   :type 'string :group 'org-mindmap)
 
-(defcustom org-mindmap-style-root
-  "concept color=black,text=white"
-  "The TikZ root style"
-  :type 'string :group 'org-mindmap)
+(defcustom org-mindmap-node-styles
+  '("shape=\"polygon\",style=\"filled\",color=\"green\""
+    "shape=\"ellipse\",style=\"filled\",color=\"grey\""
+    "shape=\"plaintext\"")
+  "A list of styles to place on the DOT nodes as they are
+generated.  The first style is placed on the root node, the
+second style is placed on first level of children and so forth
+and so on.  The last style is used for all nodes that don't have
+a style.
 
-(defcustom org-mindmap-style-level-1
-  "concept color=green!50!black"
-  "The TikZ level 1 style."
-  :type 'string :group 'org-mindmap)
-
-(defcustom org-mindmap-style-level-2
-  "concept color=blue"
-  "The TikZ level 2 style."
-  :type 'string :group 'org-mindmap)
-
-(defcustom org-mindmap-style-level-3
-  "concept color=red"
-  "The TikZ level 3 style."
-  :type 'string :group 'org-mindmap)
-
-(defcustom org-mindmap-style-level-4
-  "concept color=orange"
-  "The TikZ level 1 style."
-  :type 'string :group 'org-mindmap)
-
-(defcustom org-mindmap-style-other ""
-  "Additional TikZ style strings."
+You can also set a node style by using the DOT_NODE_STYLE
+property on a heading.  Children will inherit their parent
+heading's node style.  This allows you to style each sub-tree
+using the same style, which sometimes looks very nice."
   :type 'string :group 'org-mindmap)
 
 (defvar org-mindmap-export-buffer nil
@@ -98,63 +80,42 @@
 (defvar org-mindmap-start-level 0
   "The heading level of the root node")
 
-(defun org-mindmap-walk-tree (root)
+(defun org-mindmap-walk-tree (&optional parent)
   (let* ((title (org-no-properties (org-get-heading t)))
          (level (org-outline-level))
-         (style (org-entry-get nil "TIKZ_STYLE" t))
-         (child-style (when style (concat "[" style "]")))
-         (children (save-excursion
-                     (and (outline-next-heading) 
-                          (> (org-outline-level) level))))
-         (others (save-excursion
-                   (and 
-                    (outline-next-heading)
-                    (>= (org-outline-level) level)))))
+         (id    (or (org-entry-get nil "ID") (org-id-new nil)))
+         (style (or (org-entry-get nil "DOT_NODE_STYLE" t)
+                    (nth (- level org-mindmap-start-level) org-mindmap-node-styles)
+                    (car (last org-mindmap-node-styles)))))
     (with-current-buffer org-mindmap-export-buffer
-      (unless root (insert (concat (make-string level ?\ ) "child" child-style " {")))
-      (when (and (not root) children) (insert "\n"))
-      (when (or root children) (insert (make-string level ?\ )))
-      (when (and (not root) children) (insert " "))
-      (insert (concat "node[concept" (when style (concat "," style)) "] {" title "}")))
-    (while (and children (outline-next-heading) (= (org-outline-level) (1+ level)))
-      (org-mindmap-walk-tree nil))
-    (when (and children (org-at-heading-p))
-      (outline-previous-heading)) ;; back up, went too far
-    (with-current-buffer org-mindmap-export-buffer
-      (unless root (insert "}"))
-      (when (and (not root) others) (insert "\n")))
-    (with-current-buffer org-mindmap-export-buffer
-      (when root (insert ";\n")))))
+      (insert (concat "  \"" id "\" [label=\"" title "\""))
+      (if style (insert (concat "," style)))
+      (insert "];\n")
+      (if parent (insert (concat "  \"" parent "\" -> \"" id "\";\n"))))
+    (while (and (outline-next-heading) (= (org-outline-level) (1+ level)))
+      (org-mindmap-walk-tree id))
+    (when (and (org-at-heading-p) (< (org-outline-level) (1+ level)))
+      (outline-previous-heading))))
 
 (defun org-export-as-mindmap ()
+  "Export the current heading (as a sub-tree) as a DOT file"
   (interactive)
   (let ((file-name (or buffer-file-name "mindmap"))
         (org-mindmap-start-level (org-outline-level)))
     (setq file-name
           (if (string-match "\\..*$" file-name)
-              (replace-match ".tex" nil nil file-name)
-            (concat file-name ".tex")))
+              (replace-match ".dot" nil nil file-name)
+            (concat file-name ".dot")))
     (setq org-mindmap-export-buffer 
           (generate-new-buffer (generate-new-buffer-name "*org-mindmap*")))
     (with-current-buffer org-mindmap-export-buffer
-      (insert org-mindmap-header)
-      (insert "\n\\begin{tikzpicture}\n")
-      (insert "\\path[")
-      (insert (concat "root concept/.append style={" org-mindmap-style-root "},\n"))
-      (insert (concat " level 1 concept/.append style={" org-mindmap-style-level-1 "},\n"))
-      (insert (concat " level 2 concept/.append style={" org-mindmap-style-level-2 "},\n"))
-      (insert (concat " level 3 concept/.append style={" org-mindmap-style-level-3 "},\n"))
-      (insert (concat " level 4 concept/.append style={" org-mindmap-style-level-4 "},\n"))
-      (when (> (length org-mindmap-style-other) 0)
-        (insert (concat " " org-mindmap-style-other ",\n")))
-      (insert " mindmap,grow cyclic]\n\n"))
+      (insert org-mindmap-header))
     (save-excursion
       (org-back-to-heading)
       (save-restriction
         (org-narrow-to-subtree)
-        (org-mindmap-walk-tree t)))
+        (org-mindmap-walk-tree)))
     (with-current-buffer org-mindmap-export-buffer
-      (insert "\\end{tikzpicture}")
       (insert org-mindmap-footer)
       (write-file file-name t)
       (switch-to-buffer (current-buffer)))))
